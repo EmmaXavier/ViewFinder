522bbd12779d5a7b713932c909a471c7e4fea089
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-29 16:35:35 -0400

    boink

---
+++ ViewFinder Sharayu Veronica Ex/PhotoDetailViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDetailViewControllerTableViewCell.swift
+//  ViewFinder Sharayu Veronica Ex
+//
+//  Created by Apple on 7/29/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
